41be2ad2515cf0fcc1db2101fd18feb486d32090
Serj Sakharovskiy <8bitago@gmail.com>
2019-06-07 11:53:10 +0300

    init project

---
+++ configs/webpack.config.dev.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const devConfig = {
+  mode: 'development',
+  context: path.resolve(__dirname, '../src'),
+  entry: './index.jsx',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'static/images/[name].[ext]',
+        }
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'static/fonts/[name].[ext]'
+        }
+      },
+      {
+        test: /\.(mp3|aac)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'static/sounds/[name].[ext]'
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '../dist'),
+    historyApiFallback: true,
+    compress: true,
+    port: 9001
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './templates/index.html',
+      title: 'QST - quit smoking time!',
+    })
+  ]
+}
+
+module.exports = devConfig;
---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+      <div>
+        <h3>QST - quit smoking time!</h3>
+      </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
