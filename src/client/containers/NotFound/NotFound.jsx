91eac295e39821b27127aa0bf83f2a44ecfc4911
8bitago <eightbitago@gmail.com>
2019-09-19 10:59:28 +0300

    change style for NotFound page

--- src/client/containers/NotFound/NotFound.jsx
+++ src/client/containers/NotFound/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function NotFound() {
   return (
-    <div className="error error-notfound center">
+    <div className="uk-position-center uk-text-center">
       <h1>404 Page not found! :(</h1>
     </div>
   );
